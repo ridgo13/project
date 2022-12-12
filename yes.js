185810d1fa8df04c3129dd557875495ee74b91f2
ridgo13 <112637241+ridgo13@users.noreply.github.com>
2022-12-12 13:13:12 +0100

    Add files via upload

---
+++ yes.js
@@ -0,0 +1,554 @@
+// var x = 4;
+// var y = 20;
+// var z = 30;
+// var r = 2;
+// var n = 2;
+
+//  console.log(r + n);
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// // Addition 
+
+// console.log(x + y);
+// console.log(x + z);
+// console.log(y + z);
+
+// // Multiplication
+
+// console.log(x * y);
+// console.log(x * z);
+// console.log(z * y);
+
+// // Subtraction
+
+// console.log(x / y);
+// console.log(x / z);
+// console.log(z / y);
+
+// // Exponent 
+
+// console.log(2**2);
+// console.log(2**6);
+
+// // Modulus 
+
+// console.log(10 % 20);
+
+// var remainder = x + y;
+// console.log(remainder);
+
+// console.log("Two plus two is " + x + ' minus one thats three quick maths'); */
+
+
+//  /*var balance = 0;
+// var myApp = prompt('Welcome to Teller App.');
+// var myName = prompt('Please enter your name');
+// var account = prompt('What is your account number?');
+// var amount = prompt('How much do you want to deposit?');
+// var balance = parseInt(amount) + balance;
+
+// console.log("My name is " + myName + ", my account number is " + account + " and I want to deposit " + amount + " naira.");
+
+// var balance = confirm(
+//     balance + "has been successfully been deposited to your account, your account balance is " + amount
+// ); */
+
+// /*var balance = 0;
+// var myApp = prompt('Welcome to the Teller App.');
+// var myName = prompt('Enter your name here.');
+// var account = prompt('Type in your account number.');
+// var amount = prompt('How much would you like to deposit?');
+// var balance = parseInt(amount) + balance;
+
+// console.log("My name is " + myName + ", my account number is " + account + "and I want to deposit " + amount + "naira.");+
+
+// var balance  = confirm(
+//     balance +"has successfully been deposited to your account, your account balance is " + amount 
+// ); */
+
+
+
+
+// //var text = "The quick brown fox jumped the lazy dog what a lazy dog";
+
+// // To get the length of a string we us *.length - property
+// //var textLength = text.length;
+// //console.log(textLength);
+
+// //To change text casing to upper case we use *.toUpperCase() - method
+// //var textUpperCase = text.toUpperCase();
+// //console.log(textUpperCase);
+
+// //To change text casing to lower case we use *.toLowerCase() - method
+// //console.log(text.toLowerCase());
+
+// //To replace a value within a string with a different value we use *.replace() - method
+// //console.log(text.replace('fox', 'lion'));
+
+// //To replace all occurrences of a value within a string with a different value we use *.replaceAll() - method
+// //var newText = text.replaceAll('dog', 'cat');
+// //console.log(newText);
+
+// //var text = "Mainz 05 Football Club";
+// //console.log(parseInt(text.substring(6, 8)) + 15);
+
+// //Extract Apple from the following string "The reciple requires Apple" and log the sentence 'Apple is a fruit'
+
+// //var oldText = "The reciple requires Apple"
+
+// //var apple = oldText.substring(20, 26);
+
+// //console.log(apple + ' is a fruit');  // \n indicates a new line
+// //console.log(apple + ' is a fruit');  // \t indicates a tab
+
+// //var text = "The reciple requires Apple";
+// //var apple = text.substring(20, 26);
+
+// //console.log(apple + ' is a fruit');
+
+
+// // var friends = ["Rasta", "jefe", "Bils", "Saminu", "Sambo"];
+
+// // var Rasta = friends[0];
+// // var Sambo = friends[4];
+
+// // console.log(Rasta, Sambo);
+
+// // //Assign items to an empty array
+// // const fruits = []
+// // console.log(fruits);
+
+// // fruits[0] = 'Mango';
+// // fruits[1] = 'Orange';
+// // fruits[7] = 'Banana';
+
+// // console.log(fruits[7]);
+
+// // const someThings = ['John Smith', 16, ['Marie', 'Simon', 'Ayuba']];
+// // console.log(someThings[0]);
+// // console.log(someThings[1]);
+
+// // // Accessing items in a multi-dimensional array
+// // console.log(someThings[2][1]);
+
+// // // Create a multi-dimensional array that is 3 levels deep
+// // const someOne = ['Rodridgo', 19, ['Raeo', 'Alphy', 'Kboss', ['Koffi', 'Mane', 'Gumel']]];
+// // console.log(someOne[0]);
+// // console.log(someOne[2][0]);
+
+// // // Array properties and methods
+
+// // const pLanguages = ['JavaSript', 'Python'];
+
+// // // To check the lenght of items in a array use *.lenght
+// // console.log(pLanguages.length);
+
+// // // To remove the last time in an array use *.pop() - method
+
+// // pLanguages.pop();
+// // console.log(pLanguages);
+
+// // // To add item to an array use *.push('krmfdcnkm') - method
+// // pLanguages.push('Ada');
+// // console.log(pLanguages);
+
+// // // To remove item from the beginning of an array use *.shift() - method 
+// // const js = pLanguages.shift();
+// // console.log(pLanguages);
+// // console.log(js);
+
+// // // To inserts new elements at the start of an array, and returns the new length of the array use *.unshift() - method
+// // pLanguages.unshift('R');
+// // console.log(pLanguages);
+// // console.log(pLanguages.includes('R'));
+
+// // const text = "How are you today, hope you're doing okay?"
+// // console.log(text.split(' '));
+// // console.log(text.split('a'));
+
+// // var myFriends = prompt('Name your friends');
+// // var myFriendsLists = [];
+// // myFriendsLists = myFriends.split(', ');
+// // console.log(myFriendsLists); */
+
+
+
+// // Trasnfer the content in platforms to newPlatforms and each Platform should be converted to uppercase */
+
+// // var platforms = ['youtube', 'Twitter', 'Facebook', 'Instagram', 'Tiktok'];
+// // var newPlatforms = [];
+
+// // var youtube = platforms[0];
+// // var youtube = youtube.toUpperCase();
+// // console.log(youtube);
+
+// // var Twitter = platforms[1];
+// // var Twitter = Twitter.toUpperCase();
+// // console.log(Twitter);
+
+// // var Facebook = platforms[2];
+// // var Facebook = Facebook.toUpperCase();
+
+// // console.log(Facebook);
+
+// // var Instagram = platforms[3];
+// // var Instagram = Instagram.toUpperCase();
+
+// // console.log(Instagram);
+
+// // var Tiktok = platforms[4];
+// // var Tiktok = Tiktok.toUpperCase();
+// // console.log(Tiktok);
+
+// // var platforms = platforms.toString();
+// // console.log(platforms);
+
+// // var platforms = platforms.toUpperCase();
+// // console.log(platforms);
+
+// // var platforms = platforms.split();
+// // console.log(platforms); */
+
+    
+// // for(var i = 1; i < 101; i++) {
+// //     console.log(i);
+// // }
+
+   
+// // var platforms = ['youtube', 'Twitter', 'Facebook', 'Instagram', 'Tiktok', 'Telegram', 'Myspace', 'Tumblr'];
+
+// // for(var i = 1; i < 10; i++) {
+// //     console.log(i);
+// // }
+
+// // console.log('hello');
+
+// // for(var i = 5; i <= 25; i++) {
+// //     console.log(i);
+// // }
+
+// // for(var i = 30; i <= 260; i++) {
+// //     console.log(i);
+// // }
+
+// for (var i = 0; i < platforms.length; i++) {
+//     console.log(platforms[i]);
+// }
+
+// var ceos = [' Susan Wojcicki', ' Elon Musk', ' Mark Zukerberg', ' Kevin Syntrom', ' Shou Zi Chew', ' Pavel Durov', ' Tim Vanderhook', ' David Tarp',]
+
+// var platforms = ['youtube', 'Twitter', 'Facebook', 'Instagram', 'Tiktok', 'Telegram', 'Myspace', 'Tumblr'];
+
+// var platforms = ['locket', 'sendit', 'noteit', 'Snapchat', 'google'];
+
+
+// var ceos = [' Matt Moss', ' Michael Lawal', ' Kyle Whitehill', ' Evan Spiegel', ' Sundar Pichai'];
+
+// for(i = 0; i < platforms.length; i++ ) {
+    //     console.log(" The ceo of " + platforms[i] + ' is' + ceos[i]);
+    // }
+    
+    
+// var platforms = ['locket', 'sendit', 'noteit', 'Snapchat', 'google'];
+    
+// for( var i = 0; i < platforms.length; i++) {
+//         console.log(platforms);
+//     }
+
+// var myName = ["What is your name?", "What is your age?", "What is your state of origin?", "Enter your marital status"];
+
+// var reply = [];
+
+// for(var i = 0; i < myName.length; i++) {
+//     reply.push(prompt(myName[i]));
+// }
+
+// var a = 10;
+// var b = '10';
+
+// if (a == b) {
+//     console.log('True');
+// }
+
+// var user1 = ['ridgo13', 'pass123']; //Database
+
+// var username = prompt('Enter your username: ')
+// var password = prompt('Enter your password: ')
+
+// // User Authentication process
+// if (user1[0] === username) {
+//     if (user1[1] === password) {
+//         console.log("You're logged in!");
+//     } else {
+//         alert('The password is incorrect.')
+//     }
+// } else {
+//     alert("The username is incorrect")
+// }
+
+// Using conditionals write a program that sorts the content of the variables random in to the appropriate variable type
+
+// var random = ['set', 'list', 1, '15', 12, 13, 15, 'up'];
+// var strings = [];
+// var numbers = []; 
+
+// for(var i = 0; i < random.length; i++) {
+//     if (typeof(random[i]) === 'string') {
+//         strings.push(random[i])
+//     }
+//     if (typeof(random[i]) === 'number') {
+//         numbers.push(random[i])
+//     }
+// }
+// console.log(strings);
+// console.log(numbers);
+
+// Write a program that logs a user into an app depending on their age
+
+// If the user's age is 18 or above log him/her into the app with a welcome message
+// If the user's age is less than 18 then, display a message telling them why they can't use your app.
+
+// var user = 18;
+// var myAge = prompt("How old are you?");
+
+// if (user <= myAge) {
+//     console.log('Welcome to Locket App');}
+//     else {
+//         console.log('Sorry, you have to be 18 or above to use this app :(');
+// }
+
+//  WRITE A JAVASCRIPT FOR LOOP that will ITERATE FROM 0 TO 15.FOR EACH ITERATION IT WILL CHECK IF THE CURRENT NUMBER IS ODD OR EVEN AND DISPLAY A MESSAGE TO THE SCREEN.
+
+// var loop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+// for (var i = 0; i <= loop.length; i++) {
+//     if (i % 2 === 0) {
+//        console.log(i + ' is an even number');
+//     }
+//     else {
+//        console.log(i + ' is an odd number');
+//     }
+// }
+
+// WHILE LOOP
+
+// var x = true
+// while(x) {
+//     var value = prompt('Hit the enter key or type false to end the prog.')
+//     if(value == 'false') {
+//         x = false;
+//     }
+// }
+
+// do {
+//     console.log('Hello');
+// } while ( x == false);
+
+// var running = true;
+// var accNum = 123456789;
+// var balance = 200;
+// while(running) {
+//     var name = prompt("Welcome to TellerApp.\nEnter your full name to create an account");
+//     var amount = prompt('Enter your initial deposit: ');
+
+//     balance += parseInt(amount)
+//     alert('Your account number is: ' + accNum)
+//     alert(balance);
+//     var cont = prompt('Do you want to perform another transaction? Yes or No').toLowerCase();
+
+//     if (cont == 'yes') {
+//         var type = prompt('Choose an option\n1. Deposit\n2.withdrawal')
+//     } else {
+//         running = false
+//     }
+// }
+
+
+// If the number is divisible by 3
+// log Fizz
+
+// If the number is divisible by 5
+// log Buzz
+
+// for(i = 0; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0 ) {
+//      console.log('FizzBuzz');
+//     }
+//     if(i % 3 == 0) {
+//         console.log('Fizz');
+//     }
+//     else if(i % 5 == 0) {
+//         console.log('Buzz');
+//     }
+// }
+
+// Pizzaria
+// Welcome users to the app
+// Display the menu
+// large - $18
+// Medium - $12
+// Small - $10
+// Ask of the quantity they want
+// After the user chooses their preffered pizza size, ask them if they want to add toppings to it depending on their answer;
+// Toppings Pricing:
+// Pepperoni - $5
+// Bacon - $3
+// Extra Cheese - $2
+
+// If the pizza size is large, all toppings are $2
+// log: Your total bill is: $67
+// Check the user out and ask the user if they want to order another pizza
+
+
+
+// var welcome = prompt('Welcome to Pizzaria.');
+// var menu = prompt("This is our menu\n1 Large pizza = $18\n2 Medium pizza = $12 \n3 Small pizza = $10");
+// var quantity = prompt('How many would you like to order?');
+// var toppings = prompt('Would you like to add toppings?');
+// var toppingsPrices = prompt('1.Pepperoni - $5\n2.Bacon - $3 \n3.Extra Cheese - $2');
+
+
+// if (toppings = 'yes') {
+//     console.log(toppingsPrices);
+// }  if (toppings = 'no') {
+//     alert('Your total bill is $' + menu * quantity + toppingsPrices);
+// }
+
+// var another = prompt('Would you like to order again?');
+// if(another === 'yes') {
+//     var another = prompt(menu);
+// }
+//  if(another === 'no') {
+//     alert('Thank you for ordering with us!');
+// }
+
+// var totalPrice = 0;
+// var choice = parseInt(prompt('This is our menu\n1 Large pizza = $18\n2 Medium pizza = $12 \n3 Small pizza = $10'));
+// var quantity = parseInt(prompt('How many would you like to order?'));
+// var toppings = prompt('Would you like to add toppings?\n1 Pepperoni - $5\n2Bacon - $3 \n3Extra Cheese - $2');
+// var ordering = true;
+// while (ordering) {
+
+//     if(toppings == 1) {
+//         toppings = 5;
+//     } else if(toppings == 2) {
+//         toppings = 3
+//     } else if (toppings == 3) {
+//         toppings = 2
+//     }
+    
+//     if (choice === 1) {
+    
+//         totalPrice = (18 * quantity) + (2 * quantity);
+//     } else if (choice === 2) {
+//         totalPrice = (15 * quantity) + (toppings * quantity);
+//     } else if(choice === 3) {
+//         totalPrice = (10 * quantity) + (toppings * quantity);
+//     } else {
+//         alert('Invalid statement');
+//     }
+    
+//     var orderAgain = parseInt(prompt("Do you wish to order again\n1Yes\n2No"));
+//     if (orderAgain == 2) {
+//         ordering = false;
+//     } 
+// }
+
+// function addNumbers() {
+//     var x = 12;
+//     var y = 15;
+//     var sum = x + y;
+//     console.log(sum);
+// }
+
+// addNumbers();
+
+// function newNumbers(x,y) {
+//     console.log(x + y);
+// }
+// newNumbers(50,1003);
+
+// function greet(name) {
+//     console.log('Hello', name);
+// }
+
+// greet('Raeo!');
+// greet('Jenna!');
+
+// function addition() {
+//     for(i = 0; i < arguments.length; i++) {
+//         console.log(arguments.length);
+//     }
+// }
+// addition(1,2,3,4,5,6,7,8,9,10);
+
+
+// function evenNumbers(x) {
+//     for(i = 0; i <= x; i++) {
+//         if(i % 2 == 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+// evenNumbers(100);
+
+
+// LINKING JAVASCRIPT TO HTML
+// const button = document.querySelector('.btn');
+
+// const p = document.getElementsByTagName('p');
+
+// const h1 = document.getElementById('head');
+
+// const clickMe = document.getElementsByClassName('btn');
+
+// const paras = document.querySelectorAll('.text');
+// console.log(paras);
+
+// const i = document.querySelector('.container .btn i');
+// console.log(i);
+// const zin = document.querySelector('.zino h2');
+// console.log(zin);
+// const myButton = document.querySelector('.btn');
+// myButton.onclick = function() {
+//     myButton.style.backgroundColor = 'green';
+//     myButton.style.color = 'white';
+//     myButton.style.border = 'none';
+// }
+
+// myButton.addEventListener('click', function() {
+//     console.log("Clicked");
+// })
+
+const form = document.querySelector('.form');
+var height = document.querySelector('#height');
+var weight = document.querySelector('#weight');
+const bmiOutput = document.querySelector('.bmii');
+const remarkkOutput = document.querySelector('.remark');
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    const userHeight = parseFloat(height.value);
+    const userWeight = parseFloat(weight.value);
+    const result = userWeight / (userHeight**2);
+    const BMI = result.toFixed(2);
+
+    height.value = '';
+    weight.value = '';
+
+    bmiOutput.innerHTML = BMI
+
+    if (BMI < 18.5) {
+        remarkkOutput.innerHTML = 'You are underweight!';
+    } else if (BMI >= 18.5 || BMI < 25) {
+        remarkkOutput.innerHTML = 'You are healthy weight!';
+    } else if(BMI >= 25 || BMI < 30) {
+        remarkkOutput.innerHTML = 'You are overweight!';
+    } else if (BMI >= 30) {
+        remarkkOutput.innerHTML = 'Obesity!';
+    }
+}
+
